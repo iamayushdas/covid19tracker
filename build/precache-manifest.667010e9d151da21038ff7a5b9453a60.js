9df4f7eb0659378b347bdbdefaae2212f34dd623
iamayushdas <ayush24das@gmail.com>
2020-04-29 08:51:27 +0530

    banner added

---
+++ build/precache-manifest.667010e9d151da21038ff7a5b9453a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2521615d433840574875eb1d7d7fdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "157d69669653ff1b5d2a",
+    "url": "/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "1a79494d3ae121f5dd1a",
+    "url": "/static/js/2.a2abf0a9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.a2abf0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "157d69669653ff1b5d2a",
+    "url": "/static/js/main.fb3cb3cf.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-import { Cards, CountryPicker, Chart } from './components';
+import {  Cards, CountryPicker, Chart } from './components';
+import Banner  from './components/banner';
 import { fetchData } from './api/';
 import styles from './App.module.css';
-
+import * as Icon from 'react-feather';
 import image from './images/image.png';
+
+
+import './App.scss';
+
+
 
 class App extends React.Component {
   state = {
@@ -27,12 +33,40 @@ class App extends React.Component {
   render() {
     const { data, country } = this.state;
 
+
+
     return (
       <div className={styles.container}>
         <img className={styles.image} src={image} alt="COVID-19" />
+        <Banner />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} /> 
+       
+        <footer className="fadeInUp" style={{animationDelay: '2s'}}>
+      
+
+        <h5>We stand with everyone fighting on the frontlines</h5>
+        <div className="link">
+          <a
+            href="http://iamayushdas.me"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @iamayushdas
+          </a>
+        </div>
+        <a
+          href="https://github.com/iamayushdas/covid19tracker"
+          className="button github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon.GitHub />
+          <span>Open Sourced on GitHub</span>
+        </a>
+        
+      </footer>
       </div>
     );
   }
